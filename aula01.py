6f708f91b3e36e2d2a662c29f4b8677255348581
Carlos Vieira <carloshenriquevieirarocha12@gmail.com>
2026-02-19 10:08:11 -0300

    primeira aula de programação

---
+++ aula01.py
@@ -0,0 +1,9 @@
+idade = 16 #variavel inteira
+peso = 71,2 #variavel float
+nome = "Carlos Henrique Vieira" #variavel string
+sexo_masculino = True #variavel bool
+
+print(nome)
+print(idade)
+print(peso)
+print(sexo_masculino)
