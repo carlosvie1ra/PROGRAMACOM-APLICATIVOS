45abb93f01d3440f1c73621fe761aabd6bb286a4
carlos vieira <carloshenriquevieirarocha12@gmail.com>
2026-02-20 09:48:43 -0300

    aula 02input matematica

---
+++ aula02.input.py
@@ -0,0 +1,9 @@
+nome = input("digite seu nome de usuario: ")
+telefone = input("digite o telefone: ")
+idade = input("digite sua idade: ")
+cidade = input("informe sua cidade: ")
+
+print("OLÁ ",nome)
+print("SUA IDADE É ",telefone)
+print("SEU NUMERO É ",idade)
+print("VOCE MORA EM ",cidade)
---
+++ aula02.matematica.py
@@ -0,0 +1,14 @@
+boas_vindas =  input("digite seu nome de usuario: ")
+
+valor_inteiro1 = int(input("digite um inteiro: "))
+valor_inteiro2 = int(input("digite o segundo inteiro: "))
+
+soma = valor_inteiro1 + valor_inteiro2
+
+subtracao = valor_inteiro1 - valor_inteiro2
+
+divisao = valor_inteiro1 / valor_inteiro2
+
+print("sua soma é ",soma)
+print("sua subtração tem o resultado de ",subtracao)
+print("sua divisao e igual a ",divisao)
