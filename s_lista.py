0c14d3520b66cac70845c3bdf8f5f16f5f276053
carlos <carloshenriquevieirarocha12@gmail.com>
2026-04-09 10:04:55 -0300

    09/04

---
+++ s_lista.py
@@ -0,0 +1,9 @@
+precos = [10.50, 20.0, 5.30, 100.0]
+soma = 0
+for preco in precos:
+    soma += preco
+
+print(f"a soma de tudo é {soma}")
+
+media = nota1 + nota2 + nota3 / 3
+print(f"Sua média é {media}")
---
+++ senha_inft.py
@@ -0,0 +1,4 @@
+while True:
+    senha = input("digite a senha: ")
+    if senha == "python26":
+        print("senha correta")
